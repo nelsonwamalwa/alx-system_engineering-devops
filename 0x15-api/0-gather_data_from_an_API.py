b1ae5622a4e6ef1f3f300629334b4467bdd78829
NelsonWamalwa <wamalwanelson@gmail.com>
2024-01-31 14:06:13 +0300

    Task One

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Returning the to-do list information for a given employee ID."""
+"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/posts/1"
+    url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
