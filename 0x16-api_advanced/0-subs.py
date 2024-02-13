fbf4e14d7d96a0bf551249274928b0efc65d010a
NelsonWamalwa <wamalwanelson@gmail.com>
2024-02-13 15:59:17 +0300

    My Whole tasks sorted

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+The number of subscribers for given subreddit
+"""
+
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """
+    The function that queries all the Reddit API and returns the number 
+    of its subscribers (not all active users, total subscribers) for a 
+    given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
+
+    try:
+        return results.get('data').get('subscribers')
+
+    except Exception:
+        return 0
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+Printing all the titles of the first ten hot posts listed for given subreddit
+"""
+
+from requests import get
+
+
+def top_ten(subreddit):
+    """
+    This function that queries the Reddit API and prints the titles 
+    of the first ten hot posts listed for given subreddit
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    params = {'limit': 10}
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+
+    response = get(url, headers=user_agent, params=params)
+    results = response.json()
+
+    try:
+        my_data = results.get('data').get('children')
+
+        for i in my_data:
+            print(i.get('data').get('title'))
+
+    except Exception:
+        print("None")
